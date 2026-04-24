cdcd593f1f96d7705f0c312354c33ec9609bedd7
francois duguay <francois.duguay@epita.fr>
2026-04-24 14:55:02 +0200

    Hello world

---
+++ epitasign/epitasignApp.swift
@@ -0,0 +1,17 @@
+//
+//  epitasignApp.swift
+//  epitasign
+//
+//  Created by Guest User on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct epitasignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ epitasignTests/epitasignTests.swift
@@ -0,0 +1,16 @@
+//
+//  epitasignTests.swift
+//  epitasignTests
+//
+//  Created by Guest User on 24/04/2026.
+//
+
+import Testing
+
+struct epitasignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
